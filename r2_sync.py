4445c4154251325ff52a28d62429f8772f38bf5c
raylu <90059+raylu@users.noreply.github.com>
2025-03-15 21:24:52 -0700

    --dry-run

--- r2_sync.py
+++ r2_sync.py
@@ -7,8 +7,8 @@
 
 from __future__ import annotations
 
+import argparse
 import pathlib
-import sys
 import tomllib
 import typing
 
@@ -17,9 +17,17 @@ import boto3
 if typing.TYPE_CHECKING:
 	from mypy_boto3_s3 import S3Client
 
-VERBOSE = len(sys.argv) == 2 and sys.argv[1] == '-v'
+DRY_RUN = VERBOSE = False
 
 def main() -> None:
+	global VERBOSE, DRY_RUN
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-n', '--dry-run', action='store_true', help="don't upload/delete anything")
+	parser.add_argument('-v', '--verbose', action='store_true', help="don't be silent")
+	args = parser.parse_args()
+	DRY_RUN = args.dry_run
+	VERBOSE = args.verbose
+
 	current_dir = pathlib.Path(__file__).parent
 	with (current_dir / 'r2_sync.toml').open('rb') as f:
 		config = tomllib.load(f)
@@ -35,7 +43,8 @@ def backup_immich(r2: S3Client) -> None:
 	files = sorted(pathlib.Path('/mnt/data/immich/library/backups').iterdir())
 	log('uploading DB backup', files[-1])
 	with files[-1].open('rb') as f:
-		r2.upload_fileobj(f, 'backup', 'immich/db-backup.sql.gz')
+		if not DRY_RUN:
+			r2.upload_fileobj(f, 'backup', 'immich/db-backup.sql.gz')
 
 def backup_tree(r2: S3Client, base: pathlib.Path, r2_prefix: str) -> None:
 	'''back up base to r2_prefix'''
@@ -47,7 +56,8 @@ def backup_tree(r2: S3Client, base: pathlib.Path, r2_prefix: str) -> None:
 		if key not in objs:
 			log('uploading', rel)
 			with (base / rel).open('rb') as f:
-				r2.upload_fileobj(f, 'backup', key)
+				if not DRY_RUN:
+					r2.upload_fileobj(f, 'backup', key)
 
 def log(*o: object) -> None:
 	if VERBOSE:
